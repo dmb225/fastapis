3d3273bd1a75f69b91843dcc5b64d97b28ee1f50
dmb225 <daomalick225@gmail.com>
2025-09-24 15:19:57 +0200

    Add black, hatch, mypy, pre-commit

---
+++ src/fastapis/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root() -> dict[str, str]:
+    return {"Hello": "World"}
+
+
+def main() -> None:
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/conftest.py
@@ -0,0 +1,2 @@
+# This file is intentionally left blank.
+# It is used to share fixtures across multiple test files.
