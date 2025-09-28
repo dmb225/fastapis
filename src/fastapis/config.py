3ed259806524194b03fad9452f70e7ebce1705c5
dmb225 <daomalick225@gmail.com>
2025-09-28 09:19:12 +0000

    Configure logging

---
+++ src/fastapis/config.py
@@ -0,0 +1,7 @@
+import logging
+
+from starlette.config import Config
+
+config = Config(".env")
+
+LOG_LEVEL = config("LOG_LEVEL", default=logging.WARNING)
--- src/fastapis/main.py
+++ src/fastapis/main.py
@@ -1,7 +1,14 @@
+import logging
+
 from fastapi import FastAPI
 import uvicorn
 
-from .api import api_router
+from fastapis.api import api_router
+from fastapis.logging import configure_logging
+
+log = logging.getLogger(__name__)
+
+configure_logging()
 
 app = FastAPI(
     description="Welcome to FastAPIs' documentation! Here you will able to discover all of the ways you can interact with the APIs.",
@@ -14,4 +21,5 @@ app.include_router(api_router)
 
 
 def main() -> None:
+    log.info("Starting FastAPIs...")
     uvicorn.run("fastapis.main:app", host="0.0.0.0")
